d128ef5dcc5f9ad7e29d203b65d2d1940b4d3790
maya malka <mayamalka052@gmail.com>
2024-11-28 20:04:04 +0200

    add basic selenium project

---
+++ infraedgeTest/infraedgeTest/wikiTest.cs
@@ -0,0 +1,31 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace infraedgeTest
+{
+    public class wikiTest
+    {
+        private IWebDriver driver;
+
+        [SetUp]
+        public void SetUp()
+        {
+            driver = new ChromeDriver();
+        }
+
+        [Test]
+        public void OpenGoogle()
+        {
+            driver.Navigate().GoToUrl("https://www.google.com");
+            Assert.That(driver.Title.Contains("Google"));
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            driver.Quit();
+        }
+    }
+
+}  
